d2a8f7b9f222fb79c5f09d74c63e3d5b0c788f64
pediredlaganesh7 <ganeshpediredla123@gmail.com>
2025-12-19 21:14:49 +0530

    index.js

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -1,7 +1,6 @@
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
 import Cookies from 'js-cookie'
-
 import FiltersGroup from '../FiltersGroup'
 import ProductCard from '../ProductCard'
 import ProductsHeader from '../ProductsHeader'
